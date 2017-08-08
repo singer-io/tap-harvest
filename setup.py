cf7a9e88c071a55ef28bd7d83a5289643e3aa3c7
Mike DeLaurentis <miked@stitchdata.com>
2017-08-08 16:12:58 -0400

    bump version to 0.4.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-harvest',
-      version="0.4.6",
+      version="0.4.7",
       description='Singer.io tap for extracting data from the Harvest api',
       author='Facet Interactive',
       url='http://singer.io',
