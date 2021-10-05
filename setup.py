2852881fc169a45f7563c53dfa12e4a5e1c73dfd
Andy Lu <andy@stitchdata.com>
2021-10-05 09:31:29 -0400

    Add pytz as a dependency (#48)
    * Add pytz as a dependency
    
    * Match de version `singer-python` requires
    
    * Make pylint happy

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(name='tap-harvest',
           'singer-python==5.12.1',
           'requests==2.20.0',
           'pendulum==1.2.0',
-          'backoff==1.8.0'
+          'backoff==1.8.0',
+          'pytz==2018.4',
       ],
       entry_points='''
           [console_scripts]
