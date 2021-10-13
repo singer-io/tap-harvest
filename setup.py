502d4627b495155d87827aa5f3a02fa54db6c8ed
KrisPersonal <66801357+KrisPersonal@users.noreply.github.com>
2021-10-13 10:47:37 -0700

    Bump to version 2.1.1 (#50)
    Co-authored-by: KrishnanG <kgurusamy@talend.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-harvest',
-      version="2.1.0",
+      version="2.1.1",
       description='Singer.io tap for extracting data from the Harvest api',
       author='Facet Interactive',
       url='http://singer.io',
