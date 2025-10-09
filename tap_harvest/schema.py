636da381b8579123058cbd3fccbf42194b6c085a
satyendra101 <satyendra.singh@qlik.com>
2025-10-09 18:26:59 +0530

    SAC-28841: Add parent-tap-stream-id in metadata

--- tap_harvest/schema.py
+++ tap_harvest/schema.py
@@ -52,9 +52,9 @@ def get_schemas() -> Tuple[Dict, Dict]:
         mdata = metadata.new()
         mdata = metadata.get_standard_metadata(
             schema=schema,
-            key_properties=getattr(stream_obj, "key_properties"),
+            key_properties=getattr(stream_obj, "key_properties", None),
             valid_replication_keys=(getattr(stream_obj, "replication_keys") or []),
-            replication_method=getattr(stream_obj, "replication_method"),
+            replication_method=getattr(stream_obj, "replication_method", None),
         )
         mdata = metadata.to_map(mdata)
 
@@ -65,10 +65,16 @@ def get_schemas() -> Tuple[Dict, Dict]:
                     mdata, ("properties", field_name), "inclusion", "automatic"
                 )
 
+        # Check if the stream has any parent attribute
+        parent_attribute = getattr(stream_obj, "parent", None)
+        if parent_attribute:
+            mdata = metadata.write(mdata, (), "parent-tap-stream-id", parent_attribute)
+
         mdata = metadata.to_list(mdata)
         field_metadata[stream_name] = mdata
 
     return schemas, field_metadata
+
 
 def write_schema(stream, client, streams_to_sync, catalog) -> None:
     """Collect nested child streams to sync and write schema for selected
